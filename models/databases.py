104e610cccc4be25efc1b1ad9ba9536c5055cc54
JoaquinMorais <joaquinmorais2005@gmail.com>
2022-10-20 11:37:13 -0300

    contraseñas encriptadas, login terminado, base de datos funcional,modificar y añadir datos

--- models/databases.py
+++ models/databases.py
@@ -1,5 +1,5 @@
 from utils.db import db
-
+import crypt
 """
 class ExampleDatabase(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -21,15 +21,29 @@ class Usuarios(db.Model):
     idUsuario = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50))
     password = db.Column(db.String(50))
+    plataTotal = db.Column(db.Integer)
 
     registros = db.relationship('Registros')
 
     def __init__(self,username,password):
         self.username = username
-        self.password = password
+        self.password = crypt.crypt(password, 'salt')
+        self.plataTotal = 0
     
     def __repr__(self):
         return f'<id: {self.idUsuario}, name: "{self.username}", describe: "{self.password}">'
+
+    def plata_total(self):
+        return [{'x':x.fecha,'y':x.plataTotal} for x in self.registros]
+
+    def modificar_registro(self,monto,descripcion,tipo):
+        if (len(self.registros) >= 2):
+            self.plataTotal = self.registros[-2].plataTotal
+            self.registros[-1].modificar(monto,descripcion,tipo)
+        else:
+            self.plataTotal = 0
+            self.registros[0].modificar(monto,descripcion,'=')
+        
 
 class Registros(db.Model):
     __tablename__ = 'registros'
@@ -41,16 +55,34 @@ class Registros(db.Model):
     plataTotal = db.Column(db.Integer)
     
     idUsuario = db.Column(db.Integer, db.ForeignKey('usuarios.idUsuario'))
-    
+    usuario = db.relationship('Usuarios')
 
-
-    def __init__(self,monto,descripcion,tipo,fecha,plataTotal,idUsuario):
+    def __init__(self,monto,descripcion,tipo,fecha,idUsuario):
         self.monto = monto
         self.descripcion = descripcion
         self.tipo = tipo
         self.fecha = fecha
-        self.plataTotal = plataTotal
-        self.idUsuario = idUsuario
+        self.idUsuario = idUsuario 
+        self.plataTotal = 0
+        
+    
+    def añadir_plata(self):        
+        if self.tipo == '=':
+            self.usuario.plataTotal = self.monto
+            self.plataTotal = self.monto     
+        elif self.tipo == '+':
+            self.usuario.plataTotal += self.monto
+            self.plataTotal = self.usuario.plataTotal
+        else:
+            self.usuario.plataTotal -= self.monto
+            self.plataTotal = self.usuario.plataTotal
+    
+    def modificar(self,monto,descripcion,tipo):
+        self.monto = monto
+        self.descripcion = descripcion
+        self.tipo = tipo
+        self.añadir_plata()
+        
     
     def __repr__(self):
         return f'<Registros: {self.idRegistro}>'
--- routes/login.py
+++ routes/login.py
@@ -1,7 +1,8 @@
-from crypt import methods
 from flask import Blueprint,render_template,redirect,url_for,request,session,g,abort,flash,jsonify
-from models.databases import Usuarios
+from models.databases import Usuarios,Registros
 from utils.db import db
+from datetime import date
+import crypt
 
 Login = Blueprint("Login",__name__)
 
@@ -45,7 +46,7 @@ def singin():
 def login():
     if request.method == 'POST':
         nm = request.form['username']
-        pw = request.form['password']
+        pw = crypt.crypt(request.form['password'], 'salt')
         usuario = Usuarios.query.filter_by(username = nm).all()
         
         if len(usuario)!=0 and usuario[0].password == pw:
@@ -64,10 +65,36 @@ def login():
 @Login.route("/profile")
 def profile():
     global user
-    g.usuario = user
-    return render_template("login/profile.html")
+    if user:
+        g.usuario = user
+        return render_template("login/profile.html")
+    return redirect(url_for('Login.login'))
 
 
+@Login.route("/añadir")
+def añadir():
+    newUser = Usuarios('Admin','Admin')
+    newInstance1 = Registros(500,'auto','=','2022-03-10',1)
+    newInstance2 = Registros(1000,'chupetin','-','2022-05-21',1)
+    newInstance3 = Registros(1500,'alfajor','+','2022-08-03',1)
+    db.session.add(newUser)
+    db.session.add(newInstance1)
+    #db.session.add(newInstance2)
+    #db.session.add(newInstance3)
+    db.session.commit()
+    newInstance1.añadir_plata()
+    #newInstance2.añadir_plata()
+    #newInstance3.añadir_plata()
+    db.session.commit()
+    return 'Añadido!!'
+
+@Login.route("/mostrar")
+def mostrar():
+    user = Usuarios.query.all()[0]
+    user.modificar_registro(5,'sueldo','+')
+    db.session.commit()
+
+    return 'nashe'
 
 
 
